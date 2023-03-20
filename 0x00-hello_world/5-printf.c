93937ce2e73bb7696736cee4896ec436a95acc41
Tkem <tk@gmail.com>
2023-03-20 06:48:03 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - sends a formatted string to the standard output
+ *
+ * Return: when succesfull always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
