224c00d5de812058d1283644f92b27ebb03c4a8a
Tkem <tk@gmail.com>
2023-04-17 06:46:16 -0700

    Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - the dog
+ * @name: member 1
+ * @age: member 2
+ * @owner: member 3
+ */
+
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif /* DOG_H */
